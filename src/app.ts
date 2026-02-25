b407e745b5e500769ce73d0802fb80e76d645fd1
Pedro Miguel <pedro.miguel.e.idk@gmail.com>
2026-02-24 22:36:01 -0300

    feat: add game state

--- src/app.ts
+++ src/app.ts
@@ -1,38 +1,73 @@
-import { Animation, Button, ContainerState, Text, VRow } from "oxiui-lib";
+import { Button, ContainerState, HRow, Signal, Text, VRow } from "oxiui-lib";
+
+type STAGES = "start" | "playing" | "end";
+type CATEGORIES = ("HTML" | "CSS" | "JavaScript") | "";
+type GAME = {
+  gameStage: STAGES;
+  category: CATEGORIES;
+  questions: [];
+  currentQuestion: number;
+  score: number;
+  answerSelected: string;
+  isAnswerSelected: boolean;
+};
+
+export const GameState = new Signal<GAME>({
+  gameStage: "start",
+  category: "",
+  questions: [],
+  currentQuestion: 0,
+  score: 0,
+  answerSelected: "",
+  isAnswerSelected: false,
+});
+
+export function setGameState(state: GAME) {
+  GameState.value = {
+    ...GameState.value,
+    ...state,
+  };
+}
+
+export function startGame(category: CATEGORIES) {
+  setGameState({
+    gameStage: "playing",
+    category,
+    questions: [],
+    currentQuestion: 0,
+    score: 0,
+    answerSelected: "",
+    isAnswerSelected: false,
+  });
+}
 
 export class Application extends ContainerState {
-  $trans = 0;
-  $number = 0;
-  $redered = false;
-
-  onClick = () => {
-    if (this.$trans > 1) {
-      this.$trans = 1;
-    } else {
-      this.$trans = 100;
-    }
+  onTopicClick = (category: CATEGORIES) => {
+    startGame(category);
   };
 
   override view = () => {
+    new Text(`At ${GameState.value}`)
+      .asTitle()
+      .withStyle({ position: "absolute", left: "20px" });
+
     new VRow()
       .withProps({ id: "vrow frame" })
       .horizontalAlignment("center")
       .verticalAlignment("center")
       .body(() => {
-        new Animation()
-          .translate("horizontal", this.$trans)
-          .skew(this.$trans)
-          .transition(5, "ease-in-out")
+        new Text("Hey! Welcome to the Quizapp :D").asTitle();
+        new Text("Let's start test your web knowledge....").asH3();
+        new HRow()
+          .horizontalAlignment("center")
+          .verticalAlignment("center")
+          .withStyle({ gap: "1rem", flex: 0 })
           .body(() => {
-            new Text(`Hello, world. ${this.$trans}`).asTitle();
-            new Button("Go to 100px").onClick(this.onClick);
-          })
-          .onAppear(() => {
-            if (!this.$redered) {
-              this.onClick();
-              this.$redered = true;
-            }
-            console.log("Effect");
+            new Button("Start HTML").onClick(() => this.onTopicClick("HTML"));
+            new Button("Start CSS").onClick(() => this.onTopicClick("CSS"));
+            new Button("Start JavaScript").onClick(() =>
+              this.onTopicClick("JavaScript"),
+            );
           });
       });
   };
